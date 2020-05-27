db87cfc609af56261ae210143fc1ef0d4556d7af
Lev Neganov <glll4d@protonmail.com>
2020-05-27 22:51:30 +0300

    144

---
+++ 1_4/143.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen('https://stepik.org/media/attachments/lesson/209723/3.html').read().decode('utf-8')
+s = str(html)
+soup = BeautifulSoup(html, 'html.parser')
+
+cnt = 0
+for a in soup.find_all('td'):
+    cnt += int((a.get_text()))
+print(cnt)
