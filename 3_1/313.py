3a606c624f4af3d68ecd6673bdea97945ac70a43
glll4d <glll4d@mail.ru>
2020-05-29 03:46:52 +0300

    a

--- 3_1/313.py
+++ 3_1/313.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import xmltodict
 
 fin = open('map1.osm', 'r', encoding='utf8')
@@ -6,13 +5,4 @@ xml = fin.read()
 fin.close()
 
 parsedxml = xmltodict.parse(xml)
-=======
-import xmltodict
-
-fin = open('map1.osm', 'r', encoding='utf8')
-xml = fin.read()
-fin.close()
-
-parsedxml = xmltodict.parse(xml)
->>>>>>> f0ea5dec45d4d026e7666704cdec5baa45a4da47
 print(parsedxml['osm']['node'][100]['@id'])
